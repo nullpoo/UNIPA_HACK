589a58fd8c9ab6615680b4bfea100ab7f1c575f1
nullpoo <fmfmfmfm@gmail.com>
2012-07-03 14:46:32 +0900

    休講補講一覧まで取得
    整形はこれから

--- UNIPAHACK/MainWindow.xaml.cs
+++ UNIPAHACK/MainWindow.xaml.cs
@@ -37,9 +37,25 @@ namespace UNIPAHACK
             var req = (HttpWebRequest)WebRequest.Create("https://portal.sa.dendai.ac.jp/up/faces/login/Com00505A.jsp");
             req.CookieContainer = cc;
             var res = await req.GetResponseAsync();
-
+            var stream = res.GetResponseStream();
             var enc = System.Text.Encoding.UTF8;
-            using (var reader = new StreamReader(res.GetResponseStream(), enc))
+            using (var reader = new StreamReader(stream, enc))
+            using (var sgmlReader = new SgmlReader { InputStream = reader })
+            {
+                sgmlReader.DocType = "HTML";
+                sgmlReader.CaseFolding = CaseFolding.ToLower;
+                var doc = XDocument.Load(sgmlReader);
+                var ns = doc.Root.Name.Namespace;
+                var q = doc.Descendants(ns + "input")
+                    .Where(ul => ul.Attribute("id") != null && ul.Attribute("id").Value == "com.sun.faces.VIEW")
+                    .Select(el => el.Attribute("value").Value).ToList();
+                session_id = q[0];
+            }
+        }
+        void getSessionId(Stream stream)
+        {
+            var enc = System.Text.Encoding.UTF8;
+            using (var reader = new StreamReader(stream, enc))
             using (var sgmlReader = new SgmlReader { InputStream = reader })
             {
                 sgmlReader.DocType = "HTML";
@@ -85,15 +101,110 @@ namespace UNIPAHACK
             reqStream.Close();
 
             //HTTP GETによるクッキーの取得
-            WebResponse res = req.GetResponse();
-            Stream resStream = res.GetResponseStream();
-            Encoding enc = Encoding.GetEncoding("UTF-8");
-            StreamReader reader = new StreamReader(resStream, enc);
-            string result = reader.ReadToEnd();
-            reader.Close();
-            resStream.Close();
+            var res = await req.GetResponseAsync();
+            var enc = System.Text.Encoding.UTF8;
+            using (var reader = new StreamReader(res.GetResponseStream(), enc))
+            using (var sgmlReader = new SgmlReader { InputStream = reader })
+            {
+                sgmlReader.DocType = "HTML";
+                sgmlReader.CaseFolding = CaseFolding.ToLower;
+                var doc = XDocument.Load(sgmlReader);
+                var ns = doc.Root.Name.Namespace;
+                var q = doc.Descendants(ns + "input")
+                    .Where(ul => ul.Attribute("id") != null && ul.Attribute("id").Value == "com.sun.faces.VIEW")
+                    .Select(el => el.Attribute("value").Value).ToList();
+                session_id = q[0];
+            }
+        }
+        async Task getJyugyoInfo()
+        {
+            //パラメータ作成
+            Dictionary<string, string> dic = new Dictionary<string, string>();
+            dic.Add("form1:Poa00201A:htmlParentTable:2:htmlHeaderTbl:0:allJugyo.x","");
+            dic.Add("com.sun.faces.VIEW", session_id);
+            dic.Add("form1", "form1");
+            Uri uri = new Uri("https://portal.sa.dendai.ac.jp/up/faces/up/po/Poa00601A.jsp");
+            string param = "";
+            foreach (var item in dic.Keys)
+            {
+                param += String.Format("{0}={1}&", item, dic[item]);
+            }
+            byte[] data = Encoding.ASCII.GetBytes(param);
 
-            textbox1.Text = result;
+            //POSTリクエスト作成
+            HttpWebRequest req = (HttpWebRequest)WebRequest.Create(uri);
+            req.Method = "POST";
+            req.ContentType = "application/x-www-form-urlencoded";
+            req.UserAgent = "Mozilla/5.0 (compatible; MSIE 10.0; Windows NT 6.2; Win64; x64; Trident/6.0)";
+            req.ContentLength = data.Length;
+            req.CookieContainer = cc;
+
+            //POSTを実行
+            var reqStream = await req.GetRequestStreamAsync();
+            reqStream.Write(data, 0, data.Length);
+            reqStream.Close();
+
+            //HTTP GETによるクッキーの取得
+            var res = await req.GetResponseAsync();
+            var stream = res.GetResponseStream();
+            var enc = System.Text.Encoding.UTF8;
+            using (var reader = new StreamReader(stream, enc))
+            using (var sgmlReader = new SgmlReader { InputStream = reader })
+            {
+                sgmlReader.DocType = "HTML";
+                sgmlReader.CaseFolding = CaseFolding.ToLower;
+                var doc = XDocument.Load(sgmlReader);
+                var ns = doc.Root.Name.Namespace;
+                var q = doc.Descendants(ns + "input")
+                    .Where(ul => ul.Attribute("id") != null && ul.Attribute("id").Value == "com.sun.faces.VIEW")
+                    .Select(el => el.Attribute("value").Value).ToList();
+                session_id = q[0];
+            }
+        }
+        async Task getKyukoHoko()
+        {
+            //パラメータ作成
+            Dictionary<string, string> dic = new Dictionary<string, string>();
+            dic.Add("form1:Poa00201A:htmlParentTable:2:htmlDisplayOfAll:0:allInfoLinkCommand", "");
+            dic.Add("com.sun.faces.VIEW", session_id);
+            dic.Add("form1", "form1");
+            Uri uri = new Uri("https://portal.sa.dendai.ac.jp/up/faces/up/po/Poa00601A.jsp");
+            string param = "";
+            foreach (var item in dic.Keys)
+            {
+                param += String.Format("{0}={1}&", item, dic[item]);
+            }
+            byte[] data = Encoding.ASCII.GetBytes(param);
+
+            //POSTリクエスト作成
+            HttpWebRequest req = (HttpWebRequest)WebRequest.Create(uri);
+            req.Method = "POST";
+            req.ContentType = "application/x-www-form-urlencoded";
+            req.UserAgent = "Mozilla/5.0 (compatible; MSIE 10.0; Windows NT 6.2; Win64; x64; Trident/6.0)";
+            req.ContentLength = data.Length;
+            req.CookieContainer = cc;
+
+            //POSTを実行
+            var reqStream = await req.GetRequestStreamAsync();
+            reqStream.Write(data, 0, data.Length);
+            reqStream.Close();
+
+            //HTTP GETによるクッキーの取得
+            var res = await req.GetResponseAsync();
+            var enc = System.Text.Encoding.UTF8;
+            using (var reader = new StreamReader(res.GetResponseStream(), enc))
+            using (var sgmlReader = new SgmlReader { InputStream = reader })
+            {
+                sgmlReader.DocType = "HTML";
+                sgmlReader.CaseFolding = CaseFolding.ToLower;
+                var doc = XDocument.Load(sgmlReader);
+                var ns = doc.Root.Name.Namespace;
+                var q = doc.Descendants(ns + "input")
+                    .Where(ul => ul.Attribute("id") != null && ul.Attribute("id").Value == "com.sun.faces.VIEW")
+                    .Select(el => el.Attribute("value").Value).ToList();
+                session_id = q[0];
+                textbox1.Text = doc.ToString();
+            }
         }
 
         private async void Button_Click_1(object sender, RoutedEventArgs e)
@@ -102,6 +213,8 @@ namespace UNIPAHACK
             pass = password_textbox.Text;
             await toppage();
             await mainpage();
+            await getJyugyoInfo();
+            await getKyukoHoko();
         }
     }
 }
